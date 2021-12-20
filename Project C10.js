470a19d16a727b4ea67fa2592cc7861301f9f8e4
dariusng18 <91878330+dariusng18@users.noreply.github.com>
2021-12-20 17:56:36 +0800

    Add files via upload

--- Project C10.js
+++ Project C10.js
@@ -3,15 +3,14 @@ var seaImg,shipImg;
 
 function preload(){
   //uncomment the code to add animation to ship 
-  ship.setAnimation = "ship-1.png";
-  ship.setAnimation = "ship-2.png";
   
   
   
-  shipImg1 = loadAnimation("ship-1.png");
+  
+  shipImg = loadAnimation("ship-1.png");
   //shipImg1 = loadAnimation("ship-1.png");
   //shipImg1 = loadAnimation("ship-1");
-  //shipImg1 = loadAnimation("ship-1.png","ship-2.png","ship-1.png","ship-2.png");
+  shipImg = loadAnimation("ship-1.png","ship-2.png","ship-1.png","ship-2.png");
   //shipImg1 = loadAnimation("ship-1","ship-2","ship-1","ship-2");
   
   seaImg = loadImage("sea.png");
@@ -30,7 +29,7 @@ function setup(){
   
   
   ship = createSprite(130,200,30,30);
-  ship.addAnimation("movingShip",shipImg1);
+  ship.addAnimation("movingShip",shipImg);
   ship.scale =0.25;
   
 }
@@ -41,10 +40,10 @@ function draw() {
 
   //uncomment code to reset the background
   if(sea.x < 0){
-    sea.x = 0;
-    sea.x = sea.width;
+    //sea.x = 0;
+    //sea.x = sea.width;
     sea.x = sea.width/8;
-    sea.y = height;
+    //sea.y = height;
     
   }
   
